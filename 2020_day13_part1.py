111c5841e03e2d7f26ee1cf8e87bd337dd074032
Sau Kha <saukha@gmail.com>
2020-12-22 04:21:40 -0500

    done

--- 2020_day13_part1.py
+++ 2020_day13_part1.py
@@ -3,8 +3,8 @@
 
 # 2020 day 13 part 1
 # Examples
-#earliest = 939
-#buses = [7,13,59,31,19]
+# earliest = 939
+# buses = [7,13,59,31,19]
 
 # inputs
 earliest = 1006605
@@ -23,10 +23,4 @@ for i in range(0, len(wait)):
     if wait[i] == min(wait):
         print('answer:', buses[i]*wait[i])
 
-# part 2
-
-# inputs
-earliest = 100000000000000
-l='19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,883,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,x,x,797,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29'
-buses = l.split(',')
 
